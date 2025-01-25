68b92bb23b3a4215fc7574ce63e199343815d112
EdwardW710 <EdwardW710@yahoo.com>
2025-01-25 16:27:12 -0500

    exercise 2

--- app.js
+++ app.js
@@ -19,6 +19,54 @@ const game = {
 
   console.dir(pokemon, { maxArrayLength: null })
 
-  pokemon[59] = "Poliwag";
+// pokemon[59] = "Poliwag";
   
-  console.log(pokemon[59])
+// console.log(pokemon[59])
+
+ console.log(game)
+
+
+
+
+
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
+
+/*
+console.log(pokemon[0])
+console.log(game.party)
+game.party.push()
+console.log(game)
+/*
+
+/*
+Exercise 5
+1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+
+
+Solve Exercise 5 here:
+*/
+
+/*
+game.party.push(pokemon[1], pokemon[7], pokemon[12])
+
+console.log(game)
+*/
+
+/*
+Exercise 6
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 6 here:
+*/
+
+
