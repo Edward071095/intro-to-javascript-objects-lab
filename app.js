cfb738eb3686e09edba1560a4fd511f06ff9eb26
EdwardW710 <EdwardW710@yahoo.com>
2025-02-01 18:37:46 -0500

    exercise 6

--- app.js
+++ app.js
@@ -17,15 +17,13 @@ const game = {
     ],
   }
 
- //console.dir(pokemon, { maxArrayLength: null })
+console.dir(pokemon, { maxArrayLength: null })
+ 
+//let pokemon59 = pokemon[59];
 
- //pokemon[59] = "Poliwag";
-  
- //console.log(pokemon[59])
-
+ //console.log(pokemon59.name)
+ 
  //console.log(game)
-
-
 
 /*
 Exercise 3
@@ -53,9 +51,14 @@ Solve Exercise 4 here:
 
 
 //console.log(pokemon[0])
-//console.log(game.party)
-//game.party.push()
+//game.party.push([1])
 //console.log(game)
+
+//let starterPokemon = pokemon.find(pokemon => pokemon.starter === true);
+
+//game.party.push(starterPokemon);
+
+//console.log(game.party);
 
 
 /*
@@ -68,9 +71,9 @@ Solve Exercise 5 here:
 */
 
 
-game.party.push(pokemon[1], pokemon[7], pokemon[12])
+//game.party.push(pokemon[5], pokemon[7], pokemon[12])
 
-console.log(game)
+//console.log(game)
 
 
 /*
@@ -79,7 +82,60 @@ Exercise 6
 2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
 
 
+
 Solve Exercise 6 here:
 */
+
+let gyms = [
+  { location: "Pewter City", completed: false, difficulty: 1 },
+  { location: "Cerulean City", completed: false, difficulty: 2 },
+  { location: "Vermilion City", completed: false, difficulty: 3 },
+  { location: "Celadon City", completed: false, difficulty: 4 },
+  { location: "Fuchsia City", completed: false, difficulty: 5 },
+  { location: "Saffron City", completed: false, difficulty: 6 },
+  { location: "Cinnabar Island", completed: false, difficulty: 7 },
+  { location: "Viridian City", completed: false, difficulty: 8 },
+];
+
+
+gyms.forEach(gym => {
+  if (gym.difficulty < 3){
+    gym.completed = true;
+  }
+});
+
+console.log(gyms)
+
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+
+Solve Exercise 7 here:
+*/
+
+//game.party[0] = pokemon[1]
+
+
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 8 here:
+*/
+
+
+//console.log(game.party[game.party.name]);
+
 
 
